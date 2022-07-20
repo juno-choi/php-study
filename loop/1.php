de3f381a09e80e610893c5064eabefd7cc1340bc
juno-choi <ililil9482@naver.com>
2022-07-20 11:00:00 +0900

    반복문

---
+++ loop/1.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+	<?php
+	$i = 0;
+	while ($i < 10) {
+		echo "횟수 : ".$i."<br />";
+		$i++;
+	}
+	?>
+</body>
+</html>
---
+++ loop/2.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+	<?php
+	for ($i=0; $i < 10; $i++) { 
+		echo "2.php 횟수 : ".$i."<br />";
+	}
+	?>
+</body>
+</html>
