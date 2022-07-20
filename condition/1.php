17382b23ad820def867db962252321baf7f6224a
juno-choi <ililil9482@naver.com>
2022-07-20 10:53:01 +0900

    조건문

---
+++ condition/1.php
@@ -0,0 +1,11 @@
+<?php
+echo "시작";
+if(true){
+	echo "1";
+}else if(false){
+	echo "2";
+}else{
+	echo "3";
+}
+echo "끝";
+?>
---
+++ condition/2.php
@@ -0,0 +1,11 @@
+<?php
+echo "시작";
+if(true and false){
+	echo "1";
+}else if(false or true){
+	echo "2";
+}else{
+	echo "3";
+}
+echo "끝";
+?>
